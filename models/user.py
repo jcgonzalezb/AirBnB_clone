f15ca1bd61f364edf86bfc6082d49ed5079ecc2d
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-06 12:22:35 -0500

    user class

--- models/user.py
+++ models/user.py
@@ -9,11 +9,11 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """
     Class to create users. This class inherits from BaseModel.
-            Public class attributes:
-                email: string
-                password: string
-                first_name: string
-                last_name: string
+        Public class attributes:
+            email: string
+            password: string
+            first_name: string
+            last_name: string
     """
 
     email = ''
