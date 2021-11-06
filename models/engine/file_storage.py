06cc0451e37f54bd5c3a227717aa3a3cfb17b51f
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-06 15:14:37 -0500

    file_storage updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,8 +36,8 @@ class FileStorage:
     def reload(self):
         """Deserializes the JSON file to __objects"""
         from models.base_model import BaseModel
-        from models.place import Place
         from models.user import User
+        from models.place import Place
         from models.state import State
         from models.city import City
         from models.amenity import Amenity
