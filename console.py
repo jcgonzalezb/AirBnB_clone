d2fe39b6aa3704be87cb7bfd43598f7786ecc9ad
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-06 17:11:13 -0500

    console

--- console.py
+++ console.py
@@ -29,7 +29,7 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """will pass an empty line"""
-        return cmd.Cmd.emptyline(self)
+        pass
 
     def do_create(self, line):
         """Create an instance."""
