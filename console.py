e2d93de35e04dff74fff481d85416da3b2517cb0
Laurajcb <callejaslaura96@gmail.com>
2021-11-06 21:42:08 +0000

    fixed lines

--- console.py
+++ console.py
@@ -12,9 +12,9 @@ from models.user import User
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand defines the command interpreter."""
-    prompt = '(hbnb) '
-    classes = {"BaseModel", "User", "State", "City", "Amenity",
-               "Place", "Review"}
+
+    prompt = "(hbnb) "
+    classes = {"BaseModel", "User", "State", "City", "Amenity", "Place", "Review"}
 
     instance = []
 
@@ -27,15 +27,15 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        """will pass an empty line """
+        """will pass an empty line"""
         pass
 
     def do_create(self, line):
         """Create an instance."""
         if len(line) == 0:
-            print('** class name missing **')
+            print("** class name missing **")
         elif line not in HBNBCommand.classes:
-            print('** class doesn\'t exist **')
+            print("** class doesn't exist **")
         else:
             instance = eval(line)()
             instance.save()
@@ -44,19 +44,19 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, line):
         """Show id and classname of instance."""
         sto_object = storage.all()
-        line_split = line.split(' ')
+        line_split = line.split(" ")
         flag = True
 
         if len(line) == 0:
-            print('** class name missing **')
+            print("** class name missing **")
             return
 
         elif line_split[0] not in HBNBCommand.classes:
-            print('** class doesn\'t exist **')
+            print("** class doesn't exist **")
             return
 
         elif len(line_split) == 1:
-            print('** instance id missing **')
+            print("** instance id missing **")
             return
 
         elif len(line_split) > 0:
@@ -65,36 +65,35 @@ class HBNBCommand(cmd.Cmd):
                     print(value)
                     flag = False
             if flag is True:
-                print('** no instance found **')
+                print("** no instance found **")
                 return
 
         else:
-                instance = eval(line)()
-                instance.__str__()
-                print(instance)
+            instance = eval(line)()
+            instance.__str__()
+            print(instance)
 
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id"""
-        line_split = line.split(' ')
+        line_split = line.split(" ")
         i = line.split()
-        flag = True
 
         if len(line) == 0:
-            print('** class name missing **')
+            print("** class name missing **")
             return
 
         elif line_split[0] not in HBNBCommand.classes:
-            print('** class doesn\'t exist **')
+            print("** class doesn't exist **")
             return
 
         elif len(line_split) == 1:
-            print('** instance id missing **')
+            print("** instance id missing **")
             return
 
         sto_object = storage.all()
-        key = i[0] + '.' + i[1]
+        key = i[0] + "." + i[1]
         if key not in sto_object.keys():
-            print('** no instance found **')
+            print("** no instance found **")
             return
 
         else:
@@ -105,12 +104,12 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, line):
         """Prints all string representation of all instances
         based or not on the class name"""
-        line_split = line.split(' ')
+        line_split = line.split(" ")
         string_ins = []
         sto_object = storage.all()
 
         if line_split[0] not in HBNBCommand.classes:
-            print('** class doesn\'t exist **')
+            print("** class doesn't exist **")
             return
 
         else:
@@ -121,40 +120,42 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, line):
         """Updates an instance based on the class name
         and id by adding or updating attribute"""
+
         line_split = shlex.split(line)
         i = line.split()
         sto_object = storage.all()
-        key = i[0] + '.' + i[1]
+        key = i[0] + "." + i[1]
 
         if len(line) == 0:
-            print('** class name missing **')
+            print("** class name missing **")
             return
 
         elif line_split[0] not in HBNBCommand.classes:
-            print('** class doesn\'t exist **')
+            print("** class doesn't exist **")
             return
 
         elif len(line_split) == 1:
-            print('** instance id missing **')
+            print("** instance id missing **")
             return
 
         elif key not in sto_object.keys():
-            print('** no instance found **')
+            print("** no instance found **")
             return
 
         elif len(line_split) == 2:
             for value in sto_object.values():
                 if value.id == line_split[1]:
-                    print('** attribute name missing **')
+                    print("** attribute name missing **")
 
         elif len(line_split) == 3:
             for value in sto_object.values():
                 if value.id == line_split[1]:
-                    print('** value missing **')
+                    print("** value missing **")
 
         else:
             setattr(sto_object[key], line_split[2], line_split[3])
             storage.save()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
