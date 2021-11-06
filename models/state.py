360e92ebb7cfc06050d36dd9edb1015fac770c78
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-06 12:22:28 -0500

    state class

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Class State that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Class to create State. This class inherits from BaseModel.
+    Public class attributes:
+        name: string
+"""
+
+    name = ''
