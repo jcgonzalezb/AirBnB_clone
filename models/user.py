28be1b56877d1806efe9b8ea9641464915e8a000
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-06 09:51:43 -0500

    user updated

--- models/user.py
+++ models/user.py
@@ -3,8 +3,17 @@
 Class User that inherits from BaseModel
 """
 
-from models.basemodel import BaseModel
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
+        """
+        Class to create users. This class
+	inherits from BaseModel.
+	Public class attributes:
+		email: string
+		password: string
+		first_name: string
+		last_name: string
+        """
 
