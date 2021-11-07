7efce2b3b873855bcfeddf017dd2a4dbb70b996f
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-07 18:12:51 -0500

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -35,7 +35,6 @@ class BaseModel():
                     setattr(self, key, datetime.strptime(value, ISOFORMAT))
                 elif key != "__class__":
                     setattr(self, key, value)
-
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
