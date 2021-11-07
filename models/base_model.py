ac68180cbc370c7c1548e52dcae1dcb867474261
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-06 19:59:27 -0500

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -30,23 +30,20 @@ class BaseModel():
             datetime when an instance is created and it will
             be updated every time you change your object.
         """
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key not in ["__class__"]:
+                    setattr(self, key, value)
 
-        if not kwargs:
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(value,
+                                              "%Y-%m-%dT%H:%M:%S.%f"))
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-
-        else:
-            if kwargs is not None and not args:
-                for key, value in kwargs.items():
-                    if key not in ["__class__"]:
-                        setattr(self, key, value)
-
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key,
-                                datetime.strptime(value,
-                                                  "%Y-%m-%dT%H:%M:%S.%f"))
 
     def __str__(self):
         """
