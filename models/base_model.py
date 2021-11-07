7daa212e664c70a3ea683da19517ebfcfe05f34a
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-07 13:17:42 -0500

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,8 @@ import uuid
 from datetime import datetime
 from models import storage
 
+ISOFORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel():
     """
@@ -34,9 +36,7 @@ class BaseModel():
                 if key != "__class__":
                     setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key,
-                            datetime.strptime(value,
-                                              "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.strptime(value, ISOFORMAT))
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
