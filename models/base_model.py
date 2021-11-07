5c21c88c881ab13572cdc5715c5b21ff1d83369b
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-06 19:34:25 -0500

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -43,11 +43,8 @@ class BaseModel:
                 for key, value in kwargs.items():
                     if key in attributes:
                         if key == "created_at" or key == "updated_at":
-                            setattr(
-                                self,
-                                key,
-                                datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"),
-                            )
+                            setattr(self, key, datetime.strptime(value,
+                                    "%Y-%m-%dT%H:%M:%S.%f"))
                         else:
                             if key not in ["__class__"]:
                                 setattr(self, key, value)
