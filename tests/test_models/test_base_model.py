7247b8c79a5796dae4f907adbf1d85775bfcad18
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-08 09:35:16 -0500

    test base

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,9 +5,9 @@ Unittest for base_model.py
 import unittest
 import json
 import os
+from models import TestBaseModel
 from models import base_model
 BaseModel = base_model.BaseModel
-
 
 class TestBaseModel(unittest.TestCase):
     """This class contains several methods to test the
