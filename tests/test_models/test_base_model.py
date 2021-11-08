275cf5f9b628fe6512a3e96034d6b3fe284e2cb5
jcgonzalezb <jcgonzalezb@gmail.com>
2021-11-07 21:42:39 -0500

    test_base_model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unittest for base_model.py
+"""
+import unittest
+import json
+import os
+from models import TestBaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """This class contains several methods to test the
+    base_model.py file.
+    """
