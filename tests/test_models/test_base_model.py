446d55ca7cdbaf11d5481f05a669ab4cea0f1ba8
Laurajcb <callejaslaura96@gmail.com>
2021-11-08 21:23:22 +0000

    added test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,7 +5,7 @@ Unittest for base_model.py
 import unittest
 import json
 import os
-from models import TestBaseModel
+
 from models import base_model
 BaseModel = base_model.BaseModel
 
@@ -14,3 +14,47 @@ class TestBaseModel(unittest.TestCase):
     """This class contains several methods to test the
     base_model.py file.
     """
+    def test_class(self):
+        """Test instance of the class"""
+        base = BaseModel()
+        self.assertTrue(isinstance(base, BaseModel))
+
+    def test_id(self):
+        """Test for unic id"""
+        base1 = BaseModel()
+        base2 = BaseModel()
+        self.assertTrue(base1.id != base2.id)
+
+    def test_kwargs(self):
+        """Test instance for kwargs"""
+        base3 = BaseModel()
+        base3.name = "Betty"
+        dictionary = base3.to_dict()
+        self.assertTrue("name" in dictionary)
+
+    def test_dict(self):
+        """Test that fuction returns a dictionary."""
+        base4 = BaseModel()
+        ret_dic = base4.to_dict()
+        self.assertTrue(isinstance(ret_dic, dict))
+
+    def test_str(self):
+        """Test an obj representation of an instance"""
+        base5 = BaseModel()
+        str_base = base5.__str__()
+        self.assertTrue(str_base, str)
+
+    def test_save(self):
+        """Test it saves an instance"""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        hour = my_model.updated_at
+        my_model.save()
+        hour2 = my_model.updated_at
+        self.assertNotEqual(hour, hour2)
+        self.assertTrue(os.path.exists('file.json'))
+
+
+
+        
