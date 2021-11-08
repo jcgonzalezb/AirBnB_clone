d5bff4d652b4673a55dca27cc0a662679ba83b4b
Laurajcb <callejaslaura96@gmail.com>
2021-11-08 23:53:10 +0000

    fix tests files

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,7 +2,6 @@
 """Test City"""
 
 import unittest
-from models import TestCity
 from models import city
 City = city.City
 
@@ -15,3 +14,24 @@ class TestCity(unittest.TestCase):
         self.assertEqual(City.state_id, "")
         self.assertEqual(City.name, "")
         self.assertTrue(issubclass(City, BaseModel))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(City().updated_at))
+
+    def test_two_cities_unique_ids(self):
+        city1 = City()
+        city2 = City()
+        self.assertNotEqual(city1.id, city2.id)
+
+    def test_str(self):
+        
+        base = City()
+        base_str = base.__str__()
+        self.assertTrue(isinstance(base_str, str))    
+
+
+if __name__ == "__main__":
+    unittest.main()
