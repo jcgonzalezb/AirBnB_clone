5c80607cd3ba73daf82cb843f69a9d50c48edd99
Laurajcb <callejaslaura96@gmail.com>
2021-11-09 02:03:33 +0000

    added user test cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -45,3 +45,20 @@ class TestUser(unittest.TestCase):
         base = User()
         base_str = base.__str__()
         self.assertTrue(isinstance(base_str, str))
+
+def test_class(self):
+        """Test class"""
+        self.assertEqual(User.email, "")
+        self.assertEqual(User.password, "")
+        self.assertEqual(User.first_name, "")
+        self.assertEqual(User.last_name, "")
+        self.assertTrue(issubclass(User, BaseModel))
+
+    def test_instance(self):
+        """Test instance"""
+        my_user = User()
+        self.assertEqual(my_user.email, "")
+        self.assertEqual(my_user.password, "")
+        self.assertEqual(my_user.first_name, "")
+        self.assertEqual(my_user.last_name, "")
+        self.assertTrue(isinstance(my_user, BaseModel))
